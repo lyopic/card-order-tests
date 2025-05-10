f0d0165f51d56e3a37867fe7dd79944e190ee1db
Спирюкова валерия <L6ppaa@vk.com>
2025-05-10 20:25:24 +0300

    Первый коммит с конфигурацией проекта

--- src/test/java/ru/netology/tests/CardOrderTest.java
+++ src/test/java/ru/netology/tests/CardOrderTest.java
@@ -1,32 +1,37 @@
 package ru.netology.tests;
 
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.get("http://localhost:9999");
+    }
 
     @Test
-    void shouldSubmitCardForm() {
-        given()
-                .baseUri("https://sinsl.github.io/testing-form/") // если API доступен
-                .contentType(ContentType.JSON)
-                .body("""
-                {
-                  "name": "Иван",
-                  "surname": "Иванович",
-                  "patronymic": "Иванов",
-                  "telephone": "+7(999)999-99-99",
-                  "birthdate": "2000-11-11",
-                  "passport": "9999 99999999"
-                }
-                """)
-                .when()
-                .post("/submit") // уточни путь, если потребуется
-                .then()
-                .statusCode(200)
-                .body("success", equalTo(true));
+    void shouldSubmitFormSuccessfully() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
     }
 }
